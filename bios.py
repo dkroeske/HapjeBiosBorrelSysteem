18628845ecb1185b9b5c81c09f09b346d4cfbe5a
Diederich Kroeske <dkroeske@popd.nl>
2019-01-14 21:33:32 +0100

    Afronden 2 decimalen (Euro)

--- bios.py
+++ bios.py
@@ -15,23 +15,23 @@ if value1 == value4:
     print(name1, "great you already paid what you have to pay, \n")
 elif value1 <= value4:
     v = value4 - value1
-    print(name1, "you need to pay", v , "additionally, \n")
+    print(name1, "you need to pay", "{0:.2f}".format(v) , "additionally, \n")
 elif value1 >= value4:
     w = value1 - value4
-    print(name1, "you wiil receive", w , "additionally, \n")
+    print(name1, "you wiil receive", "{0:.2f}".format(w) , "additionally, \n")
 if value2 == value4:
     print(name2, "great you already paid what you have to pay, \n")
 elif value2 <= value4:
     x = value4 - value2
-    print(name2, "you need to pay", x , "additionally, \n")
+    print(name2, "you need to pay", "{0:.2f}".format(x) , "additionally, \n")
 elif value2 >= value4:
     y = value2 - value4
-    print(name2, "you wiil receive", y , "additionally, \n")
+    print(name2, "you will receive", "{0:.2f}".format(y) , "additionally, \n")
 if value3 == value4:
     print(name2, "great you already paid what you have to pay, \n")
 elif value3 <= value4:
     z = value4 - value3
-    print(name3, "you need to pay", z , "additionally, \n")
+    print(name3, "you need to pay", "{0:.2f}".format(z) , "additionally, \n")
 elif value3 >= value4:
     zz = value3 - value4
-    print(name3, "you wiil receive", zz , "additionally, \n")
+    print(name3, "you wiil receive", "{0:.2f}".format(zz) , "additionally, \n")
