8a6804f9db799d29070054952e7807a53bd38280
Diederich Kroeske <dkroeske@popd.nl>
2019-01-15 21:59:07 +0100

    OO rework

--- bios.py
+++ bios.py
@@ -1,37 +1,70 @@
-name1 = input("What's your name?\t")
-print("\nHi",name1)
-value1 = float(input("How much have you paid tonight (please use . for decimal)?\t"))
-name2 = input("whith whom did you go to the cinema?\t")
-print("\nHi", name2)
-value2 = float(input("How much has\t" + name2 + "\tpaid tonight?\t"))
-name3 = input("who was the third person that went to the cinema?\t")
-print("\nHi", name3)
-value3 = float(input("How much has\t" + name3 + "\tpaid tonight?\t"))
-print("\n\nSo in total the three of you spent", value1 + value2 + value3)
-value4 = (value1 + value2 + value3)/3
-print("\nSo each has to pay\t", value4)
-print("\n")
-if value1 == value4:
-    print(name1, "great you already paid what you have to pay, \n")
-elif value1 <= value4:
-    v = value4 - value1
-    print(name1, "you need to pay", "{0:.2f}".format(v) , "additionally, \n")
-elif value1 >= value4:
-    w = value1 - value4
-    print(name1, "you wiil receive", "{0:.2f}".format(w) , "additionally, \n")
-if value2 == value4:
-    print(name2, "great you already paid what you have to pay, \n")
-elif value2 <= value4:
-    x = value4 - value2
-    print(name2, "you need to pay", "{0:.2f}".format(x) , "additionally, \n")
-elif value2 >= value4:
-    y = value2 - value4
-    print(name2, "you will receive", "{0:.2f}".format(y) , "additionally, \n")
-if value3 == value4:
-    print(name2, "great you already paid what you have to pay, \n")
-elif value3 <= value4:
-    z = value4 - value3
-    print(name3, "you need to pay", "{0:.2f}".format(z) , "additionally, \n")
-elif value3 >= value4:
-    zz = value3 - value4
-    print(name3, "you wiil receive", "{0:.2f}".format(zz) , "additionally, \n")
+import re
+
+class Deelnemer:
+
+    def __init__(self, name, amount):
+        self.name = name
+        self.amount = amount
+
+class BiosBorrelCalculator:
+
+    # Initialisatie
+    def __init__(self):
+        pass
+
+    # Verzamelen wie er allemaal meedoen en met welke inleg
+    def collectNamesAndAmount(self):
+
+        self.deelnemers = []
+
+        for idx in range(3):
+            name  = input("What's your name?\t")
+            amount = float(input("Hi " + name + ", how much have you paid tonight (please use . for decimal)?\t"))
+
+            # Test voor valid name and amount input.
+            # Use regex: naam moet voldoen aan beginletter en min 3 characters
+            if re.match(r"\w[a-zA-Z]{2,}$", name) and isinstance(amount, float)  :
+                self.deelnemers.append( Deelnemer(name = name, amount = amount) )
+            else:
+                print("Ongeldige input, bye")
+                exit()
+                
+    # Verzamelen wie er allemaal meedoen en met welke inleg
+    def calculate(self):
+        # if value1 == value4:
+        #     print(name1, "great you already paid what you have to pay, \n")
+        # elif value1 <= value4:
+        #     v = value4 - value1
+        #     print(name1, "you need to pay", "{0:.2f}".format(v) , "additionally, \n")
+        # elif value1 >= value4:
+        #     w = value1 - value4
+        #     print(name1, "you wiil receive", "{0:.2f}".format(w) , "additionally, \n")
+        # if value2 == value4:
+        #     print(name2, "great you already paid what you have to pay, \n")
+        # elif value2 <= value4:
+        #     x = value4 - value2
+        #     print(name2, "you need to pay", "{0:.2f}".format(x) , "additionally, \n")
+        # elif value2 >= value4:
+        #     y = value2 - value4
+        #     print(name2, "you will receive", "{0:.2f}".format(y) , "additionally, \n")
+        # if value3 == value4:
+        #     print(name2, "great you already paid what you have to pay, \n")
+        # elif value3 <= value4:
+        #     z = value4 - value3
+        #     print(name3, "you need to pay", "{0:.2f}".format(z) , "additionally, \n")
+        # elif value3 >= value4:
+        #     zz = value3 - value4
+        #     print(name3, "you wiil receive", "{0:.2f}".format(zz) , "additionally, \n")
+        pass
+
+    def debug(self):
+        for p in self.deelnemers:
+            print( p.name + " : " + str(p.amount))
+
+#
+#
+#
+if __name__ == '__main__':
+    biosBorrel = BiosBorrelCalculator()
+    biosBorrel.collectNamesAndAmount()
+    biosBorrel.debug()
